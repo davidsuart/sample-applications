bbbe7135fab875acd7d27874ef76dfaf691ba6d7
David Suart <davidsuart@users.noreply.github.com>
2016-12-15 11:28:30 +0000

    Update server.js

--- node/server.js
+++ node/server.js
@@ -1,1 +1,19 @@
-// ...
+// Load the http module to create an http server.
+var http = require('http');
+
+// something added so we can see which instance we're hitting
+var os = require("os");
+var hostname = os.hostname();
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World\n\n");
+  response.end('(Serving from: '+hostname+' !)\n');
+});
+
+// Listen on port 8000
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server running");
